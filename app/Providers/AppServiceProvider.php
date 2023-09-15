be27c205a0d90e4a1de57ed0ef61f881f5736ba2
Soe Thura <soethura@onenex.co>
2023-09-15 23:14:26 +0630

    installed: log-viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 use Filament\Support\Facades\FilamentIcon;
+use Illuminate\Support\Facades\Auth;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,5 +34,24 @@ class AppServiceProvider extends ServiceProvider
             'panels::pages.dashboard.navigation-item' => 'icon-home',
         ]);
 
+        LogViewer::auth(function ($request) {
+
+            $authenticationHasPassed = false;
+
+            if ($request->header('PHP_AUTH_USER', null) && $request->header('PHP_AUTH_PW', null)) {
+                $username = $request->header('PHP_AUTH_USER');
+                $password = $request->header('PHP_AUTH_PW');
+
+                if ($username === config('log-viewer.basic_auth.username') && $password === config('log-viewer.basic_auth.password')) {
+                    $authenticationHasPassed = true;
+                }
+            }
+
+            if ($authenticationHasPassed === false) {
+                Auth::onceBasic();
+            }
+            
+            return $authenticationHasPassed;
+        });
     }
 }
