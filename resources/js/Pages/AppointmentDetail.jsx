64879a75d0894796e909e19a0f91b155b6fc44e3
Aung Ko Ko <alieaungko@gmail.com>
2023-09-15 23:37:46 +0630

    add appointmentStatus in AppointmentDetailUI

--- resources/js/Pages/AppointmentDetail.jsx
+++ resources/js/Pages/AppointmentDetail.jsx
@@ -7,7 +7,17 @@ export default function AppointmentDetail(props){
     return(
         <>
         <ContainerLayout>
-            <AppoinmentStatusCard />
+            <div className="pt-10"></div>
+        <div className="bg-white h-50 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <div className="flex">
+      <img src="https://i.pravatar.cc/200" alt="Your Image" className="w-1/2 h-15" style={{borderRadius: 14, height: 100, width: 100}} />
+      <div className="flex flex-col ml-4">
+        <div className="mb-2">Dr Soe Thura</div>
+        <div className="mb-2">Therapist</div>
+        <div className="mb-2">12 Years</div>
+      </div>
+    </div>
+    </div>
           <AppointmentDetailItem />
         </ContainerLayout>
         </>
--- resources/js/Pages/Components/AppointmentDetailItem.jsx
+++ resources/js/Pages/Components/AppointmentDetailItem.jsx
@@ -11,18 +11,17 @@ const FlexRow = ({ leftContent, rightContent }) => (
 
 const AppointmentDetails = () => (
   <>
-  <div className="pt-6 pl-9">
+  <div className="pt-6 items-start">
    Scheduled Appointment
     </div>
     <div className="flex flex-col items-center p-1">
-      <FlexRow leftContent="Date" rightContent="December 14, 2023" />
       <FlexRow leftContent="Time" rightContent="01:20 - 2:30" />
       <FlexRow leftContent="Patient Info" />
       <FlexRow leftContent="Full Name" rightContent="Naing Lu" />
       <FlexRow leftContent="Gender" rightContent="Male" />
       <FlexRow leftContent="Age" rightContent="28" />
     </div>
-    <div className="flex flex-col pl-9">
+    <div className="flex flex-col pl-1">
             <div>Reason</div>
             <div className="pt-2">
                 I have been experiencing persistent feelings of sadness, hopelessness, and anxiety for several months, which are affecting my daily life, relationships, and overall well-being. I believe therapy could help me better understand and manage these emotions and provide me with the tools to improve my mental health.
