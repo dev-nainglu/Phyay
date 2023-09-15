07b9db304551d95354def155356853db2aa4f771
Thura Htet Aung <thurahtetaung711@gmail.com>
2023-09-15 10:38:41 +0630

    Add: factories and seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    protected $list_of_names = [
+        'Abuse',
+        'Anxiety',
+        'Workplace',
+        'Sex Education',
+        'LGBTQ+',
+        'Youth Support',
+        'Depression',
+        'Disorder'
+    ];
+    public function run(): void
+    {
+        // add categories
+        echo "Adding categories...\n";
+        foreach ($this->list_of_names as $name) {
+            \App\Models\Category::factory()->create([
+                'name' => $name
+            ]);
+            // create 3 doctors for each category
+            \App\Models\Doctor::factory()->count(3)->create([
+                'category_id' => \App\Models\Category::where('name', $name)->first()->id
+            ]);
+        }
+    }
+}
