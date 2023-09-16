ed8bc19f2caaf900f2e14d03e497ca8e4d3664ca
Soe Thura <soethura@onenex.co>
2023-09-17 00:44:06 +0630

    Linked: appointments itmes

--- resources/js/Pages/Components/AppointmentItems.jsx
+++ resources/js/Pages/Components/AppointmentItems.jsx
@@ -2,42 +2,25 @@ import React from 'react';
 
 export default function AppointmentItems(props) {
 
-  const cards = [
-    {
-      name: 'Dr. Soe Thura',
-      numberOfPatients: '(320)',
-      rating: '4.5',
-      date: 'September 17, 2023',
-      time: '01:00 PM - 02:00 PM',
-      numberOfExperience: '12 Years',
-      fees: '3,000 MMK'   // Default rating label
-    },
-
-    {
-        name: 'Dr. Naing Lu',
-        numberOfPatients: '(320)',
-        rating: '4.5',
-        date: 'September 17, 2023',
-        time: '01:00 PM - 02:00 PM',
-        numberOfExperience: '12 Years',
-        fees: '3,000 MMK'   // Default rating label
-      }
-    // Add more card data as needed
-  ];
+  const getDate = (datetime)=> {
+    return datetime.split(" ")[0]
+  }
+  const getTime = (datetime)=> {
+    return datetime.split(" ")[1]
+  }
 
   const appointments =props.appointments;
-
   return (
     <>
             <div className="grid grid-row-1 gap-2" key="">
-            {cards.map((card, index) => (
-                 <a href={`/appointments/${index}`} key={index} className="bg-white h-50 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+            {appointments.map((card, index) => (
+                 <a href={`/appointments/${card.id}`} key={index} className="bg-white h-50 border border-gray-200 rounded-lg shadow hover:bg-gray-100">
                     <div className="flex flex-row items-center">
-                    <img src="https://i.pravatar.cc/200" alt="Your Image" className="pl-2 w-1/2 h-15" style={{borderRadius: 14, height: 100, width: 100}} />
+                    <img src={card.doctor.image_url} alt="Your Image" className="pl-2 w-1/2 h-15" style={{borderRadius: 14, height: 100, width: 100}} />
                     <div className="justify-between p-4 leading-normal">
                 <div className="justify-between leading-normal flex items-center">
                     {/* Card Title */}
-                    <h5 className="text-md font-bold tracking-tight text-gray-900" style={{ whiteSpace: 'nowrap' }}>{card.name}</h5>
+                    <h5 className="text-md font-bold tracking-tight text-gray-900" style={{ whiteSpace: 'nowrap' }}>{card.doctor.name}</h5>
                     {/* Rounded Chip View */}
                     <div className="bg-yellow-200 px-2 py-1 rounded-full ml-2 text-xs text-yellow-800">
                         Upcoming
@@ -49,7 +32,7 @@ export default function AppointmentItems(props) {
                                 <path d="M12.6667 1.33333H12V0.666667C12 0.298667 11.702 0 11.3333 0C10.9647 0 10.6667 0.298667 10.6667 0.666667V1.33333H5.33333V0.666667C5.33333 0.298667 5.03533 0 4.66667 0C4.298 0 4 0.298667 4 0.666667V1.33333H3.33333C1.49533 1.33333 0 2.82867 0 4.66667V12.6667C0 14.5047 1.49533 16 3.33333 16H12.6667C14.5047 16 16 14.5047 16 12.6667V4.66667C16 2.82867 14.5047 1.33333 12.6667 1.33333ZM3.33333 2.66667H12.6667C13.7693 2.66667 14.6667 3.564 14.6667 4.66667V5.33333H1.33333V4.66667C1.33333 3.564 2.23067 2.66667 3.33333 2.66667ZM12.6667 14.6667H3.33333C2.23067 14.6667 1.33333 13.7693 1.33333 12.6667V6.66667H14.6667V12.6667C14.6667 13.7693 13.7693 14.6667 12.6667 14.6667ZM12.6667 9.33333C12.6667 9.70133 12.3687 10 12 10H4C3.63133 10 3.33333 9.70133 3.33333 9.33333C3.33333 8.96533 3.63133 8.66667 4 8.66667H12C12.3687 8.66667 12.6667 8.96533 12.6667 9.33333ZM8 12C8 12.368 7.702 12.6667 7.33333 12.6667H4C3.63133 12.6667 3.33333 12.368 3.33333 12C3.33333 11.632 3.63133 11.3333 4 11.3333H7.33333C7.702 11.3333 8 11.632 8 12Z" fill="#3F3E3E"/>
                             </svg>
                             <p className="font-normal text-gray-700 dark:text-gray-400" style={{ fontSize: '12px', marginLeft: '8px' }}>
-                                {card.date}
+                                {getDate(card.appointment_start_date)}
                             </p>
                         </div>
                         <div className="pt-3 my-1 flex items-center">
@@ -64,7 +47,7 @@ export default function AppointmentItems(props) {
                                 </defs>
                             </svg>
                             <p className="font-normal text-gray-700 dark:text-gray-400" style={{ fontSize: '12px', marginLeft: '8px' }}>
-                                {card.time}
+                                {getTime(card.appointment_start_date)}
                             </p>
                         </div>
                     </div>
