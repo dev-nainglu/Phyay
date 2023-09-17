23ffb984e3f97677383a9b1d409e3b6fec9baed0
Naing Lu <nainglu@nexlabs.co>
2023-09-17 09:31:08 +0630

    done

--- resources/js/Pages/AppointmentDetail.jsx
+++ resources/js/Pages/AppointmentDetail.jsx
@@ -2,6 +2,7 @@ import AppointmentDetailItem from './Components/AppointmentDetailItem';
 import ContainerLayout from './ContainerLayout';
 import AppointmentItems from './Components/AppointmentItems';
 import AppoinmentStatusCard from './Components/AppoinmentStatusCard';
+import Title from './Components/Title';
 
 
 export default function AppointmentDetail(props){
--- resources/js/Pages/Components/AppointmentDetailItem.jsx
+++ resources/js/Pages/Components/AppointmentDetailItem.jsx
@@ -1,5 +1,19 @@
 export default function AppointmentDetail(props){
-    console.log(props)
+    function copyText(){
+        var copyText = document.getElementById("default-search");
+
+        // Select the text field
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); // For mobile devices
+
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(copyText.value);
+    }
+
+    function gohome(){
+        window.location.href = "/"
+    }
+
     return(
         <>
 
@@ -69,16 +83,21 @@ export default function AppointmentDetail(props){
                                     <p className="text-sm font-medium text-gray-900 truncate">
                                         Meeting Link
                                     </p>
-                                        <a href="https://us04web.zoom.us/j/74163162756?pwd=Ec2qoH6SSFGLy1XpCPNDcuHFbb8xba.1" class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                                        https://us04web.zoom.us/j/74163162756?pwd=Ec2qoH6SSFGLy1XpCPNDcuHFbb8xba.1
-                                        <svg class="w-4 h-4 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                        </svg>
-                                        </a>
+                                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                                        <div class="relative">
+                                            <input type="search" id="default-search" value="https://us04web.zoom.us/j/74163162756?pwd=Ec2qoH6SSFGLy1XpCPNDcuHFbb8xba.1" class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos..." disabled />
+                                            <button type="submit" onClick={() => copyText()} class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M6 11C6 8.17157 6 6.75736 6.87868 5.87868C7.75736 5 9.17157 5 12 5H15C17.8284 5 19.2426 5 20.1213 5.87868C21 6.75736 21 8.17157 21 11V16C21 18.8284 21 20.2426 20.1213 21.1213C19.2426 22 17.8284 22 15 22H12C9.17157 22 7.75736 22 6.87868 21.1213C6 20.2426 6 18.8284 6 16V11Z" stroke="#1C274C" stroke-width="1.5"/>
+                                            <path opacity="0.5" d="M6 19C4.34315 19 3 17.6569 3 16V10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H15C16.6569 2 18 3.34315 18 5" stroke="#1C274C" stroke-width="1.5"/>
+                                            </svg>
+                                            </button>
+                                        </div>
+
                                 </div>
                             </div>
                         </li>
                     </ul>
+                    <a type="submit" onClick={() => gohome()} className="w-full mb-10 text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Done</a>
             </div>
             </div>
 
